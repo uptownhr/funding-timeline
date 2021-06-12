6bf63e22cd326a4b06c235620991888ba53a49c0
uptown <uptownhr@gmail.com>
2021-06-12 16:56:17 -0700

    created getPHStartups

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,12 +1,43 @@
 import { AppService } from './app.service';
+import { GraphQLClient } from 'graphql-request';
 
 describe('app service', () => {
+  const client = new GraphQLClient(
+    'https://api.producthunt.com/v2/api/graphql',
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.PH_TOKEN}`,
+      },
+    },
+  );
+  const app = new AppService(client);
+
   describe('getPHStartups', () => {
-    const app = new AppService();
-    it('returns an array of startup responses from product hunts graphql query', () => {
+    const spyRequest = jest.spyOn(client, 'request');
+
+    beforeEach(async () => {
+      spyRequest.mockReset();
+    });
+
+    it('returns an array of startup responses from product hunts graphql query', async () => {
+      spyRequest.mockResolvedValueOnce({
+        posts: {
+          edges: [
+            {
+              id: 1,
+              name: '1',
+            },
+            {
+              id: 2,
+              name: '2',
+            },
+          ],
+        },
+      });
+
       const res = app.getPHStartups();
 
-      expect(res.length).toBeGreaterThan(0);
+      await expect(res).resolves.toEqual(expect.any(Array));
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { request, gql, GraphQLClient } from 'graphql-request';
+
+interface test {
+  name: string;
+}
 
 @Injectable()
 export class AppService {
-  @Cron('* * * * * *')
-  getHello(): string {
-    console.log('hello world', process.env.PH_TOKEN);
-    return 'Hello World!';
+  constructor(private readonly phClient: GraphQLClient) {}
+  @Cron('0 * * * * *')
+  async getHello() {
+    const startups = await this.getPHStartups();
+    return startups;
   }
 
-  getPHStartups() {
-    return [false];
+  async getPHStartups() {
+    const query = gql`
+      {
+        posts {
+          edges {
+            node {
+              id
+              name
+            }
+          }
+        }
+      }
+    `;
+
+    try {
+      const res = await this.phClient.request(query);
+      return res.posts.edges;
+    } catch (e) {
+      console.log('error', e);
+    }
   }
 }
