58e72a4ecc7b6ae2729ccc73d029e8eb5f3ff1ef
uptown <uptownhr@gmail.com>
2021-06-13 06:51:40 -0700

    using config service for key

--- src/phClient.ts
+++ src/phClient.ts
@@ -1,12 +1,14 @@
 import { GraphQLClient } from 'graphql-request';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 const PRODUCT_HUNT_API_URL = 'https://api.producthunt.com/v2/api/graphql';
-const PRODUCT_HUNT_API_TOKEN = process.env.PH_TOKEN;
 
 @Injectable()
 export class ProductHuntClient extends GraphQLClient {
-  constructor() {
+  constructor(conf: ConfigService) {
+    const PRODUCT_HUNT_API_TOKEN = conf.get('PH_TOKEN');
+
     super(PRODUCT_HUNT_API_URL, {
       headers: {
         Authorization: `Bearer ${PRODUCT_HUNT_API_TOKEN}`,
